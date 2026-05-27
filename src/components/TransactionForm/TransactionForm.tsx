94d857b0998d7acbeec19e96d7a2043464ce83bd
kelvin <kellzer01@gmail.com>
2026-05-27 08:59:39 -0300

    feat: style TransactionForm

--- src/components/TransactionForm/TransactionForm.tsx
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -1,5 +1,7 @@
 import { useState, type FormEvent, type ChangeEvent} from 'react'
 import type { Transaction } from '../../types'
+import './TransactionForm.css'
+
 
 
 
@@ -24,29 +26,31 @@ function TransactionForm({ onAdd }: TransactionFormProps) {
         setType('income')
     }
   return (
-    <div>
-      <h2>Add Transaction</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
+    <div className="form-container">
+      <h2 className="form-title">Add Transaction</h2>
+      <form onSubmit={handleSubmit} className="transaction-form">
+        <label className="form-group">
           Name:
           <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} />
         </label>
-        <label>
+        <label className="form-group">
           Value:
           <input type="number" name="value" value={value} onChange={(e) => setValue(Number(e.target.value))} />
         </label>
-        <label>
+        <label className="form-group">
           Date:
           <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)} />
         </label>
-        <label>
+        <label className="form-group">
           Type:
           <select name="type" value={type} onChange={(e: ChangeEvent<HTMLSelectElement>) => setType(e.target.value as 'income' | 'expense')}>
             <option value="income">Income</option>
             <option value="expense">Expense</option>
           </select>
         </label>
-        <button type="submit">Add Transaction</button>
+        <button type="submit" className="form-button">
+          Add Transaction
+        </button>
       </form>
     </div>
   )
